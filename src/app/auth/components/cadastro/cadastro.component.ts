143f2c80d989860f16742d393fde7b20968b9275
Danrleydev <danrleysouza53@gmail.com>
2022-10-11 10:35:31 -0300

    tela cadastro

--- src/app/auth/components/cadastro/cadastro.component.ts
+++ src/app/auth/components/cadastro/cadastro.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AbstractControl, FormBuilder, ValidationErrors, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-cadastro',
@@ -6,8 +7,35 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./cadastro.component.scss']
 })
 export class CadastroComponent implements OnInit {
+  hide = true
+  
+  constructor(private fb: FormBuilder) { }
 
-  constructor() { }
+  signupForm = this.fb.group(
+    {
+      nome: ['', [Validators.required]],
+      nick: ['', [Validators.required]],
+      email: ['', [Validators.required, Validators.email]],
+      senha: ['', [Validators.required, Validators.minLength(8)]],
+      confirma_senha: [''],
+      recaptcha: ['', Validators.required],
+    },
+    { validators: [this.matchPasswords] }
+  );
+
+  matchPasswords(control: AbstractControl): ValidationErrors | null {
+    return control.get('senha')!.value !== control.get('confirma_senha')!.value
+      ? { matchPasswords: true }
+      : null;
+  }
+  
+  onSubmit(){
+
+  }
+
+  onLoginGoogle(){
+
+  }
 
   ngOnInit(): void {
   }
